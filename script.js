f386d1a4fb47556d1f96d7c80c9eecb69a2b1464
zamckinn150 <70042061+zamckinn150@users.noreply.github.com>
2020-09-21 11:08:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var number = 5
+
+var usernumber = prompt ("Please enter number");
+
+while(isNaN(usernumber)==true){
+ var usernumber = prompt ("Please enter number");
+}
+var numberInt=parseInt(number);
+var usernumberInt=parseInt(usernumber);
+var finalnumber= numberInt + usernumberInt;
+
+if(finalnumber>=10);{
+  document.body.querySelector(".end").innerHTML = "Program is over"
+}
+//while
+for(var i=0; i<5; i++){
+(finalnumber<10)
+var usernumber = prompt ("Please enter number");
+ if(finalnumber>=10);{
+ document.body.querySelector(".end").innerHTML = "Program is over"
+}
+}
